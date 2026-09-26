af852d0a0507632869a89635aa0527cae34ee966
Veeveehian <viviandyke0@gmail.com>
2026-09-25 22:58:40 -1200

    text

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Founder from '../../assets/Founder.png'
 import NavBar from '../../Components/NavBar/NavBar'
 import Footer from '../../Components/Footer/Footer'
-import "./AboutUs.Css"
+import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
@@ -35,7 +35,7 @@ const AboutUs = () => {
              learning, still experimenting, and still discovering new things every <br/>
              day. This first website may not be perfect, but it represents where I <br/>
              started — and how far I'm willing to go. And this is only the beginning.</p>
-             <h3>Dike O. Vivian.</h3>
+             <h3>Dike O. Vivian</h3>
              <p>viviandyke0@gmail.com</p>
         </div>
         </div>
