319564be7aa94a6f8484bce5faaf4a2f9c0ebcb3
Veeveehian <viviandyke0@gmail.com>
2026-09-25 22:09:47 -1200

    i added agenda

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Agenda1 from '../../assets/Agenda1.jpg'
+import Agenda2 from '../../assets/Agenda2.jpg'
+import Agenda3 from '../../assets/Agenda3.jpg'
+import Agenda4 from '../../assets/Agenda4.jpg'
+import Agenda5 from '../../assets/Agenda5.jpg'
 
 const Services = () => {
   return (
@@ -8,23 +13,27 @@ const Services = () => {
         <div className='agendaContainer'>
         <div className='agenda1'>
           <h3>Quality Education</h3>
-           {/* <img src={card1} alt="alt" /> */}
+           <img src={Agenda1} alt="alt" />
           <p>To provide students with quality, accessibile, and practical education that prepared them for the future.</p>
         </div>
         <div className='agenda2'>
           <h3>Student Development</h3>
+           <img src={Agenda2} alt="alt" />
           <p>To encourage students to develop their academic, creative, leadership, and interpersonal skills.</p>
         </div>
         <div className='agenda3'>
           <h3>Innovation & Technology</h3>
+           <img src={Agenda3} alt="alt" />
           <p>To embrace modern technology and innovative methods of learning to make education more engaging and effective.</p>
         </div>
         <div className='agenda4'>
           <h3>Character & Leadership</h3>
+           <img src={Agenda4} alt="alt" />
           <p>To build responsible, condifent, and innovative method of learning to make education more engaging and effective.</p>
         </div>
         <div className='agenda5'>
           <h3>Community & Excellence</h3>
+           <img src={Agenda5} alt="alt" />
           <p>To create an inclusive learning community where students and staff work together toward continous growth and excellence.</p>
         </div>
         </div>
